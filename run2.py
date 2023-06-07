d5e48816ca3a4ce7b291f7e09f02adc553e351d5
faysal505 <127836350+faysal505@users.noreply.github.com>
2023-06-07 13:29:24 +0600

    Add files via upload

--- run2.py
+++ run2.py
@@ -439,9 +439,9 @@ def results():
             admin.balance = admin.balance - admin.rate
             db.session.commit()
             render = render_template("nid.html", data=person, image=source)
-            htmlToPdf(render, person['nameEn'])
-            return send_from_directory('static', person['nameEn'] + '.pdf', as_attachment=True)
-            # return render
+            return render
+            # htmlToPdf(render, person['nameEn'])
+            # return send_from_directory('static', person['nameEn'] + '.pdf', as_attachment=True)
         else:
             return render_template("home.html", balance=admin.balance, rate=admin.rate, alert='<script>alert("Not Enough Balance")</script>')
 
